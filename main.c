95d10c9c115c6fd7cca8c4afc788a893d34780c3
노원석 <wonseokroh@dano.me>
2020-12-21 16:49:00 +0900

    update sandglass inclination

--- main.c
+++ main.c
@@ -71,17 +71,18 @@ int main()
 #define F_CPU 16000000UL
 // ATmega128의 레지스터 등이 정의되어 있음
 #include <avr/io.h>
-
 // _delay_ms() 함수 등이 정의되어 있음
 #include <util/delay.h>
+#include <stdio.h>
+#include <stdbool.h>
 #define __DELAY_BACKWARD_COMPATIBLE_ _ // for Atmel Studio 6
 void buzzer(int hz, int count);
-void sand_animation();
+void start_sandglass();
 void timer_end_animation();
 
 // C 언어의 주 실행 함수
 int main(void){
-	sand_animation();
+	// start_sandglass();
 	// [buzzer test]
 	// int i;
 	// DDRB = 0x10;
@@ -94,18 +95,14 @@ int main(void){
 	// }
 
 	// [기울기 센서 test]
-	// DDRA = 0xff; // 출력모드
-	// DDRG = 0x00; // 입력모드
-	// while(1){
-	// 	if(PING & 0b00000010){
-	// 		//전기 통했을 떄
-	// 		PORTA = 0b10000000;
-	// 	}else{
-	// 		//전기 안통하고 있을 떄
-	// 		PORTA = 0b00000001;
-	// 	}
-	// }
+	DDRA = 0xff; // 출력모드
+	DDRG = 0x00; // 입력모드
+	while(1){
+		start_sandglass();
+	}
 }
+
+
 void buzzer(int hz, int count) {
 	int i, j, ms;
 	ms = 500/400;
@@ -117,7 +114,9 @@ void buzzer(int hz, int count) {
 		_delay_ms(ms);
 	} 
 }
-void sand_animation(){
+
+
+void start_sandglass(){
 	unsigned char i; // 8비트의 변수 선언
 	unsigned char j;
 	unsigned long count;
@@ -240,6 +239,12 @@ void sand_animation(){
 	DDRA = 0b11111111;
 	DDRD = 0b11111111;
 
+	bool is_electricity_on = false;
+	if(PING & 0b00000010){
+		//전기 통하고 있는 상태 모래시계 작동중
+		is_electricity_on = true;
+	}
+
 	for (j = 0; j < 11; j++)
 	{
 		while (count < 30)
@@ -248,8 +253,21 @@ void sand_animation(){
 			for (i = 0; i < 8; i++)
 			{
 				// 8비트의 ROWS 신호 중 1비트만 논리 1로 출력
-				PORTA = (0b10000000 >> (i));
-
+				if (is_electricity_on)
+				{
+					PORTA = (0b10000000 >> (i));
+					if(!(PING & 0b00000010)){
+						//전기 안통하게 될 때
+						return;
+					}
+				}else{
+					PORTA = (0b00000001 << (i));
+					if(PING & 0b00000010){
+						//전기 통하게 될 때
+						return;
+					}
+				}
+				
 				// COLS 신호에 데이터 출력
 				PORTD = COLS[j][i];
 
@@ -265,10 +283,21 @@ void sand_animation(){
 	// return 1;
 }
 
+
 void timer_end_animation(){
 	unsigned char i; // 8비트의 변수 선언
-	unsigned char j;
+
 	char COLS[11][8] = {
+		{
+			0b00000000,
+			0b10000001,
+			0b11000011,
+			0b11100111,
+			0b11111111,
+			0b11111111,
+			0b11111111,
+			0b11111111,
+		},
 		{
 			0b11111111,
 			0b11111111,
@@ -278,175 +307,45 @@ void timer_end_animation(){
 			0b11000011,
 			0b10000001,
 			0b00000000,
-		},
+		}
 	};
+	
+	//전기 안통하고 있는 상태 모래시계 작동중
+	bool is_electricity_on = false;
+	int electricity_num = 0;
+	if(PING & 0b00000010){
+		//전기 통하고 있는 상태 모래시계 작동중
+		is_electricity_on = true;
+		electricity_num = 1;
+	}
+
 	while (1)
 	{
-		for (j = 0; j < 1; j++)
+		// 8줄의 LED 매트릭스가 켜지도록 동작을 반복함
+		for (i = 0; i < 8; i++)
 		{
-			// 8줄의 LED 매트릭스가 켜지도록 동작을 반복함
-			for (i = 0; i < 8; i++)
-			{
-				// 8비트의 ROWS 신호 중 1비트만 논리 1로 출력
-				PORTA = (0b10000000 >> (i));
+			// 8비트의 ROWS 신호 중 1비트만 논리 1로 출력
+			PORTA = (0b10000000 >> (i));
+			
+			// COLS 신호에 데이터 출력
+			PORTD = COLS[electricity_num][i];
 
-				// COLS 신호에 데이터 출력
-				PORTD = COLS[j][i];
-
-				_delay_ms(2); // 2ms 동안 대기
+			_delay_ms(2); // 2ms 동안 대기
+		}
+		
+		if (is_electricity_on)
+		{
+			if(!(PING & 0b00000010)){
+				//전기 안통하게 될 때
+				break;
+			}
+		}else{
+			if(PING & 0b00000010){
+				//전기 통하게 될 때
+				break;
 			}
 		}
+		
 	}
 	
 }
-/*int main(void)
-{
-	unsigned char i; // 8비트의 변수 선언
-	unsigned char j;
-	unsigned long count;
-
-	count = 0;
-	// LDM(8*8)에 표시할 모양 비트들을 저장(삼각형 모양)
-	char COLS[11][8] = {
-		{
-			0b00000000,
-			0b10000001,
-			0b11000011,
-			0b11100111,
-			0b11111111,
-			0b11111111,
-			0b11111111,
-			0b11111111,
-		},
-		{
-			0b00011000,
-			0b10000001,
-			0b11000011,
-			0b11100111,
-			0b11111111,
-			0b11111111,
-			0b11111111,
-			0b11100111,
-		},
-		{
-			0b00111100,
-			0b10000001,
-			0b11000011,
-			0b11100111,
-			0b11111111,
-			0b11111111,
-			0b11111111,
-			0b11000011,
-		},
-		{
-			0b01111110,
-			0b10000001,
-			0b11000011,
-			0b11100111,
-			0b11111111,
-			0b11111111,
-			0b11111111,
-			0b10000001,
-		},
-		{
-			0b11111111,
-			0b10000001,
-			0b11000011,
-			0b11100111,
-			0b11111111,
-			0b11111111,
-			0b11111111,
-			0b00000000,
-		},
-		{
-			0b11111111,
-			0b10011001,
-			0b11000011,
-			0b11100111,
-			0b11111111,
-			0b11111111,
-			0b11100111,
-			0b00000000,
-		},
-		{
-			0b11111111,
-			0b10111101,
-			0b11000011,
-			0b11100111,
-			0b11111111,
-			0b11111111,
-			0b11000011,
-			0b00000000,
-		},
-		{
-			0b11111111,
-			0b11111111,
-			0b11000011,
-			0b11100111,
-			0b11111111,
-			0b11111111,
-			0b10000001,
-			0b00000000,
-		},
-		{
-			0b11111111,
-			0b11111111,
-			0b11011011,
-			0b11100111,
-			0b11111111,
-			0b11100111,
-			0b10000001,
-			0b00000000,
-		},
-		{
-			0b11111111,
-			0b11111111,
-			0b11111111,
-			0b11100111,
-			0b11111111,
-			0b11000011,
-			0b10000001,
-			0b00000000,
-		},
-		{
-			0b11111111,
-			0b11111111,
-			0b11111111,
-			0b11111111,
-			0b11100111,
-			0b11000011,
-			0b10000001,
-			0b00000000,
-		},
-	};
-
-	DDRA = 0b11111111;
-	DDRD = 0b11111111;
-
-	// while 블록 안의 문장을 무한 반복
-	while (1)
-	{
-		for (j = 0; j < 11; j++)
-		{
-			while (count < 30)
-			{
-				// 8줄의 LED 매트릭스가 켜지도록 동작을 반복함
-				for (i = 0; i < 8; i++)
-				{
-					// 8비트의 ROWS 신호 중 1비트만 논리 1로 출력
-					PORTA = (0b10000000 >> (i));
-
-					// COLS 신호에 데이터 출력
-					PORTD = COLS[j][i];
-
-					_delay_ms(2); // 2ms 동안 대기
-				}
-				count++;
-			}
-			count = 0;
-		}
-	}
-
-	// 함수의 형태와 같이 정수형(int)의 값을 반환함
-	return 1;
-}*/
